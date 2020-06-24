58dd3633bc1c1cbd3aedae8177bc28e9e639d333
SergeySedov73 <sedov_ul@mail.ru>
2020-06-24 23:27:33 +0300

    add string

---
+++ src/GB/L1/HW/Main.java
@@ -0,0 +1,14 @@
+package GB.L1.HW;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Hello world!" + i);
+        }
+        int s;
+        s = 1;
+
+        String a = "string";
+    }
+}
